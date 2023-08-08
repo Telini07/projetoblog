5a807c058e5aa5f76a61e8f53c25daf2342229a9
Telini07 <brunotelinirocha51@gmail.com>
2023-08-08 12:06:56 -0300

    0808

--- tests/sql_test.php
+++ tests/sql_test.php
@@ -2,9 +2,9 @@
     require_once '../core/sql.php';
 
     $id = 1;
-    $nome = 'murilo';
-    $email = 'murilo@gmail.com';
-    $senha = '123mudar';
+    $nome = 'bruno';
+    $email = 'brunotelinirocha@gmail.com';
+    $senha = 'murilocirilo';
     $dados = ['nome' => $nome,
               'email' => $email,
               'senha' => $senha];

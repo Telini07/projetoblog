fef1497691ef52a02c8768d83f37eae17e0de66f
Telini07 <brunotelinirocha51@gmail.com>
2023-08-08 12:05:39 -0300

    0808

---
+++ core/mysql.php
@@ -0,0 +1,40 @@
+<?php
+
+function insere (string $entidade, array $dados) : bool
+{
+    $retorno = false;
+
+    foreach ($dados as $campo => $dado){
+        $coringa[$campo] = '?';
+        $tipo[] = gettype($dado)[0];
+        $$campo = $dado;
+    }
+
+    $instrucao = insert($entidade, $coringa);
+
+    $conexao = conecta();
+
+    $stmt = mysqli_prepare($conexao, $instrucao);
+
+    eval('mysqli_stmt_bind_param($stmt, \'' . implode('',$tipo) . '\',$' . implode(', $', array_keys($dados)) . ');');
+
+    mysqli_stmt_execute($stmt);
+
+    $retorno = (boolean) mysqli_stmt_affected_rows($stmt);
+
+    $_SESSION['errors'] = mysqli_stmt_error_list($stmt);
+
+    mysqli_stmt_close($stmt);
+
+    desconecta($conexao);
+
+    return $retorno;
+}
+
+function atualiza(string $entidade, array $dados, array $criterio = []) : bool
+{
+    $retorno = false;
+
+    foreach ($dados as $campo =>)
+}
+?>

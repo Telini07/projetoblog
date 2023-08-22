423433701f3672dd769bc87c5dc3e0730a5f7f8a
Telini07 <brunotelinirocha50@gmail.com>
2023-08-22 11:15:41 -0300

    2208

--- pagina_teste.php
+++ pagina_teste.php
@@ -6,20 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>POST | Projeto para Web com PHP</title>
     <link rel="stylesheet"
-    href="../lib/">
+    href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <?php
-                    include 'topo.php';
+                    include 'includes/topo.php';
                 ?>
         </div>
     </div>
     <div class="row" style="min-height: 500px;">
         <div class="col-md-12">
-            <?php include 'menu.php'; ?>
+            <?php include 'includes/menu.php'; ?>
         </div>
         <div class="col-md-10" style="padding-top: 50px;">
             <h2>Pagina teste includes</h2>
@@ -27,12 +27,12 @@
         <div class="row">
             <div class="col-md-12">
                 <?php
-                include 'rodape.php';
+                include 'includes/rodape.php';
                 ?>
             </div>
         </div>
 
     </div>
-    <script src="../lib/bootsrap-4.2.1-dist/js"></script>
+    <script src="lib/bootsrap-4.2.1-dist/js"></script>
 </body>
 </html>
